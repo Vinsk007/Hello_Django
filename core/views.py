066e545137a5c8dcde7653ae0d515698b338d6fa
Vinsk007 <rodrigo.vinskoski@gmail.com>
2019-12-08 13:35:06 -0300

    Commit do 1o App Django. Retorna 'Hello World'.

---
+++ core/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+def hello(request, nome, idade):
+    #return HttpResponse('<h1>Hello World</h1>')
+    return HttpResponse('<h1>Hello {} de {} anos<h1/>'.format(nome, idade)) #Passando parâmetros no endereço e aparecendo no txt da url
+
